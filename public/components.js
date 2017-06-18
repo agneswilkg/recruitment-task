813fbd2a93529786f4e19d14280be7f8f394ec05
agneswilkg <wilkmademe@gmail.com>
2017-06-18 14:16:11 +0200

    create first draft to build on

--- public/components.js
+++ public/components.js
@@ -1,5 +1,56 @@
 Vue.component('todo-item', {
-    template: '#bleh', //require('template.html'),
+    template: '#bleh',
+    props: ['title']
+})
+
+new Vue({
+    el: '#to-do-list',
+    data: {
+        newTodoText: '',
+        todos: [
+            'wake up',
+            'take Gapcio for a walk',
+            'program',
+            'go to sleep'
+        ]
+    },
+    methods: {
+        addNewTodo: function() {
+            this.todos.push(this.newTodoText)
+            this.newTodoText = ''
+        }
+    }
+})
+
+
+
+/*
+new Vue ({
+  el: '#app',
+  data: {
+    todos: {
+      value: '',
+      done: true
+    },
+  },
+  created(){
+    this.getToDos()
+    console.log(this.data)
+  },
+  methods: {
+    getToDos: function() {
+      this.$http.get('http://127.0.0.1:3000/todo').then(function(response) {
+        console.log(response.body)
+      this.data = response.body
+    });
+    }
+  }
+})
+
+
+
+Vue.component('todo-item', {
+    template: '#bleh',
     props: ['todo']
 })
 
@@ -7,7 +58,10 @@ Vue.component('todo-item', {
 new Vue({
   el: '#app',
   data: {
-    items: [],
+    todo: {
+      'value': '',
+      'done': false
+    },
   },
   created() {
     this.getToDos()
@@ -17,6 +71,16 @@ new Vue({
       this.$http.get('http://127.0.0.1:3000/todo').then(function (response) {
       this.data = response.data
     });
+    postToDo: function () {
+      this.$http.post('http://127.0.0.1:3000/todo', {"vaue": todo.value, "done": true})
+    }
     }
   }
 })
+
+
+this.$http.post('http://demo8159500.mockable.io/post/check', {
+              domain: this.domain
+            }, function (data, status, request) {
+                this.postResults = data;
+*/
