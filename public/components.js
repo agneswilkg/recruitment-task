19698e98346b5f53ebdac461c1a28811dc595ced
agneswilkg <wilkmademe@gmail.com>
2017-06-27 23:19:42 +0200

    Add toggle done logic

--- public/components.js
+++ public/components.js
@@ -35,6 +35,15 @@ new Vue({
           this.$http.delete('http://127.0.0.1:3000/todo', {body: {'id': id}}).then(function(r){
               this.todos.splice(index, 1)
           })
+        },
+        putTodo: function(todo) {
+          var value = todo.value;
+          var done = todo.done;
+          var id = todo['_id']
+          console.log(value, done, !done, id);
+          this.$http.put('http://127.0.0.1:3000/todo', {'value': value, 'done': !done, 'id': id}).then(function(){
+
+          })
         }
     }
 })
