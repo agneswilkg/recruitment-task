6b21c1d9335b3276714162f908c4658bf174904e
agneswilkg <wilkmademe@gmail.com>
2017-06-27 12:21:13 +0200

    Add optimistic update(?...) to post method

--- public/components.js
+++ public/components.js
@@ -14,7 +14,6 @@ new Vue({
     },
     created(){
       this.getToDos()
-      console.log(this.data)
     },
     methods: {
         getToDos: function() {
@@ -27,72 +26,10 @@ new Vue({
             var val = this.todo.value
             this.$http.post('http://127.0.0.1:3000/todo', {"value": val}).then (function(response) {
               console.log(response.body);
+              this.todos.push({value: val, done: false});
               this.getToDos()
             });
-            //this.todos.push({value: val, done: false})
             this.todo.value = null
         }
     }
 })
-
-
-/*
-new Vue ({
-  el: '#app',
-  data: {
-    todos: {
-      value: '',
-      done: true
-    },
-  },
-  created(){
-    this.getToDos()
-    console.log(this.data)
-  },
-  methods: {
-    getToDos: function() {
-      this.$http.get('http://127.0.0.1:3000/todo').then(function(response) {
-        console.log(response.body)
-      this.data = response.body
-    });
-    }
-  }
-})
-
-
-
-Vue.component('todo-item', {
-    template: '#bleh',
-    props: ['todo']
-})
-
-
-new Vue({
-  el: '#app',
-  data: {
-    todo: {
-      'value': '',
-      'done': false
-    },
-  },
-  created() {
-    this.getToDos()
-  },
-  methods: {
-    getToDos: function () {
-      this.$http.get('http://127.0.0.1:3000/todo').then(function (response) {
-      this.data = response.data
-    });
-    postToDo: function () {
-      this.$http.post('http://127.0.0.1:3000/todo', {"vaue": todo.value, "done": true})
-    }
-    }
-  }
-})
-
-
-this.$http.post('http://demo8159500.mockable.io/post/check', {
-              domain: this.domain
-            }, function (data, status, request) {
-                this.postResults = data;
-*/
